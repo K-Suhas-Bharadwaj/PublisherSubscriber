1c4513d1613248c3b34a5e313c9366ddb589d083
Suhas Bharadwaj K <tak.kumara@gmail.com>
2023-01-17 21:03:31 +0530

    Update Value.java

--- src/Value.java
+++ src/Value.java
@@ -1,9 +1,13 @@
 public class Value<Generic> {
+
     Generic value;
+
     public Value(Generic value) {
         this.value = value;
     }
+
     public Generic getValue() {
         return value;
     }
+
 }
