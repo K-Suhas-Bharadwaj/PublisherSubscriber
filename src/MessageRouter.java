93e0828c0731c0d07a93de3b329d70ffaab6e83c
Suhas Bharadwaj K <tak.kumara@gmail.com>
2023-01-18 12:53:07 +0530

    Update MessageRouter.java

--- src/MessageRouter.java
+++ src/MessageRouter.java
@@ -14,7 +14,7 @@ public class MessageRouter {
     }
 
     public void stream(Message message) {
-        Set<Services> subscribersToMessageTopic = message.getTopic().getSubscribers();
+        Set<Service> subscribersToMessageTopic = message.getTopic().getSubscribers();
         for(Service service : services) {
             if(subscribersToMessageTopic.contains(service)) {
                 service.getInbox().add(message);
