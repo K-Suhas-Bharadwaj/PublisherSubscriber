e4edd7c920ce484083bc6f0c96093e3c228bcdf4
Suhas Bharadwaj K <tak.kumara@gmail.com>
2024-02-03 12:15:37 +0530

    Create ExecTimes.java

---
+++ ExecTimes.java
@@ -0,0 +1,24 @@
+private int[] getTotalExecutionTime(int n, String[] logs) {
+        int[] execTimes = new int[n];
+        int currFuncId = -1;
+        int currFuncStart = -1;
+        int lastFuncId = -1;
+        int lastFuncStart = -1;
+        for(String log : logs) {
+            StringTokenizer st = new StringTokenizer(log, ":");
+            int id = Integer.parseInt(st.nextToken());
+            String action = st.nextToken();
+            int timeStamp = Integer.parseInt(st.nextToken());
+            if(action.equals("Start")) {
+                if(currFuncId != -1) execTimes[currFuncId] += timeStamp - currFuncStart;
+                lastFuncId = currFuncId;
+                currFuncId = id;
+                currFuncStart = timeStamp;
+            } else {
+                execTimes[currFuncId] += timeStamp - currFuncStart + 1;
+                currFuncId = lastFuncId;
+                currFuncStart = timeStamp + 1;
+            }
+        }
+        return execTime;
+    }
